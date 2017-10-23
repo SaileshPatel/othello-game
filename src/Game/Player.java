91f35842d2ec40db993ae7d21d4d1f2649e67b9f
Sailesh Patel <patelsa3@aston.ac.uk>
2017-10-23 14:35:00 +0100

    added methods to player

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -1,15 +1,32 @@
 package Game;
 
+/**
+ * The player class interacts with the Game view the GameView Control 
+ * 
+ * Take gameview as Parameter
+ * @author Sailesh Patel
+ * @author Zak Hirsi
+ * @since 23/10/2017
+ * @version 0.0.1
+ *
+ */
 public class Player implements Participant{
-	public Player(){}
-
-	public boolean ready() {
-		// TODO Auto-generated method stub
-		return false;
+	
+	private GameViewControl gvc;
+	
+	// wait for text box to be implemented
+	
+	public Player(GameViewControl gvc){
+		// the player has a game view controller
+		this.gvc = gvc;
 	}
-
-	public Position getPosition() {
-		// TODO Auto-generated method stub
-		return null;
+	
+	
+	// set position method
+	// when there is a move, use getPosition
+	// game will use setPosition 
+	
+	public void setPosition(){
+		
 	}
 }
