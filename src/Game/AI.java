878f18faf8fb6537b3ecc8ead341023f3f491417
kozinaa <kozinaa@aston.ac.uk>
2017-10-23 12:00:38 +0100

    Update AI.java

--- src/Game/AI.java
+++ src/Game/AI.java
@@ -1,7 +1,12 @@
 package Game;
 
 public class AI implements Participant{
-	public AI(){}
+    private Tree<Data> tree;
+    
+	public AI(){
+	    Node<Data> root = new Node<Data>(new Data(board, whiteTurn)); // we need the board position and who's turn it is
+		tree = new Tree<Data>(root);
+	}
 
 	@Override
 	public boolean ready() {
