d3fc239d53f710cb595dbdabb8463be407bdb134
bergj2 <bergj2@aston.ac.uk>
2017-11-22 23:48:57 +0000

    made Participant a functional interface

--- src/othello/game/player/Participant.java
+++ src/othello/game/player/Participant.java
@@ -7,6 +7,7 @@ import othello.game.GameSession;
  * @since 	??/10/2017
  * @version 20/11/2017
  */
+@FunctionalInterface
 public interface Participant
 {
 	public void notifyTurn(final GameSession session);
