bfd122709448e00e68e4d7b212a987ac9c9ef65f
James Shorthouse <james.shorthouse@outlook.com>
2017-11-29 13:26:12 +0000

    Initial implementation of GameSession

--- core/src/com/othellog4/screens/GameScreen.java
+++ core/src/com/othellog4/screens/GameScreen.java
@@ -1,23 +1,44 @@
 package com.othellog4.screens;
 
+import java.util.Observer;
+
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.othellog4.Othello;
 import com.othellog4.graphics.BoardRenderer;
-
+import com.othellog4.game.GameSession;
 import com.othellog4.game.board.GameBoard;
+import com.othellog4.game.player.Participant;
+import com.othellog4.game.player.Player;
 
-public class GameScreen extends ScreenAdapter {
+public class GameScreen extends ScreenAdapter implements Observer {
 	Othello game;
 	SpriteBatch spriteBatch;
 	BoardRenderer boardRenderer;
+	GameSession session;
+	ParticipantProxy[] participants;
+	ParticipantProxy[] localParticipants;
+	PlayerKey[] keys;
 	
-	public GameScreen(Othello game, GameBoard board) {
-		this.game = game;
+	
+	public GameScreen(GameSession session, ParticipantKey[] keys) {
+		this.session = session;
+		this.participants = session.getParticipants(); 
+		
+		localParticipants = new ParticpantProxy[keys.length];
+		
+		for(int i=0; i<localParticipants.length; i++) {
+			localParticipants[i] = keys[i].getPlayer();
+		}
+		
 		spriteBatch = game.getSpriteBatch();
 		
 		//Board created in main menu
 		boardRenderer = new BoardRenderer(spriteBatch, board);
+	}
+	
+	public void updatedGameSession(GameSession gamesession) {
+		this.gamesession = gamesession;
 	}
 	
 	public void update() {
