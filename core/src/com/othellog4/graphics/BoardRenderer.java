4ee17aa0d86fed7dfe8eb1fd768144f63817115d
JShorthouse <james.shorthouse@outlook.com>
2017-12-06 13:21:03 +0000

    Added getPosUnderMouse() method along with optimization and some cleanup

--- core/src/com/othellog4/graphics/BoardRenderer.java
+++ core/src/com/othellog4/graphics/BoardRenderer.java
@@ -9,6 +9,8 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.othellog4.game.GameSession;
@@ -40,6 +42,13 @@ public class BoardRenderer {
 	private float boardBackgroundX;
 	private float boardBackgroundY;
 	private float boardBackgroundWidth;
+	
+	// X and Y positions to draw pieces from
+	// Note that this is the bottom left of the piece
+	private float pieceXPositions[];
+	private float pieceYPositions[];
+	
+	private Position posUnderMouse;
 
 	private ProxyGameBoard board;
 
@@ -68,6 +77,9 @@ public class BoardRenderer {
 		viewport.apply();
 
 		this.board = (ProxyGameBoard) board;
+		
+		pieceXPositions = new float[board.size()];
+		pieceYPositions = new float[board.size()];
 
 		boardWidth = GAME_WORLD_HEIGHT - (2 * boardPadding);
 		boardSize = board.size();
@@ -81,14 +93,19 @@ public class BoardRenderer {
 		boardBackgroundWidth = GAME_WORLD_HEIGHT - (2 * boardBackgroundPadding);
 		boardBackgroundX = (GAME_WORLD_WIDTH / 2) - (boardBackgroundWidth / 2);
 		boardBackgroundY = (GAME_WORLD_HEIGHT / 2) - (boardBackgroundWidth / 2);
-
-		System.out.println(startingPosY);
+		
+		generatePieceCoordinates();
 
 	}
 
 	public void resize(int width, int height) {
 		viewport.update(width, height);
 		cam.position.set(GAME_WORLD_WIDTH / 2, GAME_WORLD_HEIGHT / 2, 0);
+	}
+	
+	public void update() {
+		updatePosUnderMouse();
+		//System.out.println(posUnderMouse);
 	}
 
 	public void render() {
@@ -122,14 +139,6 @@ public class BoardRenderer {
 			shape.rect(startingPosX,(startingY + (y*columnWidth) - (lineWidth/2)),boardWidth,lineWidth);
 			
 		}
-		System.out.println(boardBackgroundX + " " + boardBackgroundY);
-		
-		
-//		for (int x = 0; x<boardSize;x++){
-//			for (int y = 0;boardSize;y++)
-//			
-//		}
-		
 		
 		shape.end();
 
@@ -140,7 +149,7 @@ public class BoardRenderer {
 				
 				
 				Texture actualPiece = null;
-				Optional<Piece> optional = board.view(new Position(x, y));
+				Optional<Piece> optional = board.view(Position.at(x, y));
 				
 				if (optional.isPresent() == false) {
 					actualPiece = emptyPiece;
@@ -155,16 +164,83 @@ public class BoardRenderer {
 						break;
 
 					}
-					spriteBatch.draw(actualPiece, startingPosX + piecePaddingActual + (columnWidth * x),
-							startingPosY - piecePaddingActual - (columnWidth * y) - pieceSizeActual, pieceSizeActual,
-							pieceSizeActual);
-
 				}
-
+				spriteBatch.draw(actualPiece, pieceXPositions[x],
+						pieceYPositions[y], pieceSizeActual, pieceSizeActual);
 			}
 		}
 
 		spriteBatch.end();
 
+	}
+	
+	/**
+	 * Return the board position of the piece the mouse is currently hovering over
+	 * @return Position
+	 */
+	public Position getPosUnderMouse() {
+		return posUnderMouse;
+	}
+	
+	/**
+	 * Update the board position of the piece the mouse is currently hovering over
+	 * Used internally for rendering piece highlights
+	 */
+	private void updatePosUnderMouse() {
+		Vector2 mousePos = getUnprojectedMousePos();
+		//System.out.println(mousePos);
+		Integer xPos = null;
+		Integer yPos = null;
+		
+		//Find if within x bounds of piece
+		for(int x = 0; x<pieceXPositions.length; x++) {
+			if(mousePos.x >= pieceXPositions[x] && 
+					mousePos.x <= pieceXPositions[x] + pieceSizeActual) {
+				xPos = x;
+				break;
+			}
+		}
+		if(xPos == null) {
+			posUnderMouse = null;
+			return;
+		}
+		
+		//Find if within y bounds of piece
+		for(int y = 0; y<pieceYPositions.length; y++) {
+			if(mousePos.y >= pieceYPositions[y] && 
+					mousePos.y <= pieceYPositions[y] + pieceSizeActual) {
+				yPos = y;
+				break;
+			}
+		}
+		if(yPos == null) {
+			posUnderMouse = null;
+			return;
+		} else {
+			posUnderMouse = Position.at(xPos, yPos);
+			return;
+		}
+	}
+	
+	/**
+	 * Get the mouse position within the world space
+	 * @return Vector2 mouse position
+	 */
+	private Vector2 getUnprojectedMousePos() {
+		Vector3 mouseActualPos = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+		viewport.unproject(mouseActualPos);
+		return new Vector2(mouseActualPos.x, mouseActualPos.y);
+	}
+	
+	/**
+	 * Update piece position coordinate arrays
+	 */
+	private void generatePieceCoordinates() {
+		for(int x=0; x<pieceXPositions.length; x++) {
+			pieceXPositions[x] = startingPosX + piecePaddingActual + (columnWidth * x);
+		}
+		for(int y=0; y<pieceYPositions.length; y++) {
+			pieceYPositions[y] = startingPosY - piecePaddingActual - (columnWidth * y) - pieceSizeActual;
+		}
 	}
 }
