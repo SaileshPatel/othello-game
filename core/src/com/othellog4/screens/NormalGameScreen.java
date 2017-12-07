e03e34344d7338280f07cc74e40d59d2fefe6bfd
bergj2 <bergj2@aston.ac.uk>
2017-12-07 18:34:04 +0000

    Added the NormalGameScreen class for real this time!

---
+++ core/src/com/othellog4/screens/NormalGameScreen.java
@@ -0,0 +1,33 @@
+package com.othellog4.screens;
+
+import com.othellog4.Othello;
+import com.othellog4.game.GameModel;
+import com.othellog4.game.board.Position;
+
+/**
+ * 
+ * @author 	159014260 John Berg
+ * @since 	07/12/2017
+ * @version 07/12/2017
+ */
+public final class NormalGameScreen extends GameScreen
+{
+
+	public NormalGameScreen(
+			final GameModel model,
+			final Othello game)
+	{
+		super(model, game);
+	}
+	@Override
+	protected boolean checkInput(Position position)
+	{
+		return true;
+	}
+
+	@Override
+	protected void postRender()
+	{
+	}
+
+}
