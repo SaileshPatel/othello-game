96d3ea6ca1f7881001c5b6fdc91cdf3ae3d17f83
bergj2 <bergj2@aston.ac.uk>
2017-12-12 13:54:11 +0000

    Added the functionality to display an icon

--- desktop/src/com/othellog4/desktop/DesktopLauncher.java
+++ desktop/src/com/othellog4/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.othellog4.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.othellog4.Othello;
@@ -9,6 +10,7 @@ public class DesktopLauncher {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.height = 720;
 		config.width = 1280;
+		config.addIcon("icon/icon.png", Files.FileType.Internal);
 		new LwjglApplication(new Othello(), config);
 	}
 }
