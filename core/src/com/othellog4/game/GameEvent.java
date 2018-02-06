3da37e629c18845961c0e3451284f408bea26c95
bergj2 <bergj2@aston.ac.uk>
2018-02-06 14:58:20 +0000

    Documented GameEvent

--- core/src/com/othellog4/game/GameEvent.java
+++ core/src/com/othellog4/game/GameEvent.java
@@ -1,16 +1,22 @@
 package com.othellog4.game;
 
 /**
- * 
- * 
+ * The {@code GameEvent} enumeration is a listing of events which may occur
+ * during a {@link Game}.
  * 
  * @author 	159014260 John Berg
  * @since	25/01/2018
- * @version 25/01/2018
+ * @version 06/02/2018
  */
 public enum GameEvent
 {
+	/**
+	 * Signal the beginning of playing the game.
+	 */
 	BEGIN,
+	/**
+	 * Signal the pausing of the game.
+	 */
 	PAUSED,
 	/**
 	 * Signal the end of a game.
