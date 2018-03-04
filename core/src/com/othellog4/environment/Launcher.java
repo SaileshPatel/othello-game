7573890f210ac2229b7d4f3e0c9b8876d5128b9b
bergj2 <bergj2@aston.ac.uk>
2018-03-04 14:48:38 +0000

    Some new class additions

---
+++ core/src/com/othellog4/environment/Launcher.java
@@ -0,0 +1,163 @@
+package com.othellog4.environment;
+
+import com.othellog4.game.Game;
+import com.othellog4.game.GameModel;
+import com.othellog4.game.board.GameBoard;
+
+/**
+ * The {@code Launcher} class is a singleton which is responsible for setting
+ * up games, and taking care of the details involved during the setup phase.
+ * 
+ * <p>
+ * The {@code Launcher} also provides the facilities to suspend and resume
+ * a single {@link GameModel}.
+ * </p>
+ * 
+ * @author 	159014260 John Berg
+ * @since	03/03/2018
+ * @version 04/03/2018
+ */
+public class Launcher
+{
+	//=========================================================================
+	//Static fields.
+	/**
+	 * The singleton instance of the {@code Launcher} class.
+	 * 
+	 * <p>
+	 * Lazily initialised.
+	 * </p>
+	 * 
+	 * @see Launcher#get()
+	 */
+	private static Launcher instance = null;
+	//=========================================================================
+	//Fields.
+	/**
+	 * The {@link GameModel} which is cached by <code>this</code>
+	 * {@code Launcher} object.
+	 * 
+	 * @see GameModel
+	 */
+	private GameModel onHold;
+	//=========================================================================
+	//Constructors.
+	/**
+	 * Create the {@code Launcher} object.
+	 * 
+	 * <p>
+	 * Private to restrict access to creation.
+	 * </p>
+	 */
+	private Launcher()
+	{
+		onHold = null;
+	}
+	//=========================================================================
+	//Methods.
+	/**
+	 * Check if the {@code Launcher} currently has a {@link GameModel} cached.
+	 * 
+	 * @return <code>true</code> if a {@link GameModel} is cached, otherwise,
+	 * 			returns <code>false</code>.
+	 */
+	public final synchronized boolean hasOnHold()
+	{
+		return onHold != null;
+	}
+	/**
+	 * Clear the cached {@link GameModel}.
+	 */
+	public final synchronized void clear()
+	{
+		onHold = null;
+	}
+	/**
+	 * Cache a {@link GameModel} object which can be resumed later.
+	 * 
+	 * <p>
+	 * The {@code Launcher} only has capacity for one {@link GameModel} and
+	 * will overwrite the currently cached {@link GameModel} if it exists.
+	 * </p>
+	 * 
+	 * @param model The {@link GameModel} to cache.
+	 * @throws NullPointerException If <code>model</code> is <code>null</code>.
+	 * @see GameModel
+	 */
+	public final void hold(final GameModel model)
+			throws
+			NullPointerException
+	{
+		if(model == null)
+			throw new NullPointerException();
+		synchronized(this)
+		{
+			onHold = model;
+		}
+	}
+	/**
+	 * Retrieve the cached {@link GameModel} from <code>this</code>
+	 * {@code Launcher} object.
+	 * 
+	 * <p>
+	 * Retrieving the {@link GameModel} will clear the cached
+	 * {@link GameModel}.
+	 * </p>
+	 * 
+	 * @return The {@link GameModel} which was cached by <code>this</code>
+	 * 			{@code Launcher}.
+	 * @throws IllegalStateException If <code>this</code> {@code Launcher}
+	 * 			contains no cached {@link GameModel}.
+	 * @see GameModel
+	 */
+	public final synchronized GameModel resume()
+			throws
+			IllegalStateException
+	{
+		if(!hasOnHold())
+			throw new IllegalStateException();
+		final GameModel model = onHold;
+		clear();
+		return model;
+	}
+	/**
+	 * Create a {@link GameModel} for a new local game.
+	 * 
+	 * @param p1 The {@link PlayerType} which is the first player.
+	 * @param p2 The {@link PlayerType} which is the second player.
+	 * @param mode The {@link Mode} of the new {@link GameModel}.
+	 * @return The {@link GameModel} of the new game.
+	 * @see PlayerType
+	 * @see Mode
+	 * @see GameModel
+	 */
+	public GameModel newGame(
+			final PlayerType p1,
+			final PlayerType p2,
+			final Mode mode)
+	{
+		return new GameModel(
+				new Game(new GameBoard(8)),
+				p1.get(),
+				p2.get(),
+				mode.get());
+	}
+	//=========================================================================
+	//Static methods.
+	/**
+	 * Get the instance of the {@code Launcher} class.
+	 * 
+	 * <p>
+	 * This method may initialise the {@code Launcher} instance if it does not
+	 * exist.
+	 * </p>
+	 * 
+	 * @return The only instance of the {@code Launcher} class.
+	 */
+	public static final synchronized Launcher get()
+	{
+		return instance == null
+				?instance = new Launcher()
+				:instance;
+	}
+}
---
+++ core/src/com/othellog4/environment/PlayerType.java
@@ -0,0 +1,115 @@
+package com.othellog4.environment;
+
+import com.othellog4.game.player.AutomaticPlayer;
+import com.othellog4.game.player.Participant;
+import com.othellog4.game.player.Player;
+import com.othellog4.game.player.ai.DelayStrategies;
+import com.othellog4.game.player.ai.EvaluationStrategies;
+import com.othellog4.game.player.ai.SearchStrategies;
+
+/**
+ * The {@code PlayerType} enumeration is the collection of players which can
+ * be selected when creating a new game.
+ * 
+ * @author 	159014260 John Berg
+ * @since 	03/03/2018
+ * @version 03/03/2018
+ */
+public enum PlayerType
+{
+	//=========================================================================
+	//Enum constants.
+	/**
+	 * The {@code PlayerType} object which represents a human player.
+	 * 
+	 * <p>
+	 * Generates {@link Player} objects.
+	 * </p>
+	 */
+	USER
+	{
+		@Override
+		final Participant get()
+		{
+			return new Player();
+		}
+	},
+	/**
+	 * The {@code PlayerType} object which represents an AI of the easiest
+	 * difficulty.
+	 * 
+	 * <p>
+	 * Provides access to a shared instance of an {@link AutomaticPlayer}
+	 * object.
+	 * </p>
+	 */
+	AI_EASY
+	{
+		private final Participant ai = new AutomaticPlayer(
+				null,
+				SearchStrategies.RANDOM_SELECTION,
+				DelayStrategies.WAIT_ONE_SEC);
+		@Override
+		final Participant get()
+		{
+			return ai;
+		}
+	},
+	/**
+	 * The {@code PlayerType} object which represents an AI of medium
+	 * difficulty.
+	 * 
+	 * <p>
+	 * Provides access to a shared instance of an {@link AutomaticPlayer}
+	 * object.
+	 * </p>
+	 */
+	AI_MEDIUM
+	{
+		private final Participant ai = new AutomaticPlayer(
+				EvaluationStrategies.COUNT,
+				SearchStrategies.BEST_IMMIDIATE,
+				DelayStrategies.WAIT_ONE_SEC);
+		@Override
+		final Participant get()
+		{
+			return ai;
+		}
+	},
+	/**
+	 * The {@code PlayerType} object which represents an AI of the hardest
+	 * available difficulty.
+	 * 
+	 * <p>
+	 * Provides access to a shared instance of an {@link AutomaticPlayer}
+	 * object.
+	 * </p>
+	 */
+	AI_HARD
+	{
+		@Override
+		final Participant get()
+		{
+			return null;
+		}
+	};
+	//=========================================================================
+	//Abstract methods.
+	/**
+	 * Get the {@link Participant} object which is corresponding to the
+	 * {@code PlayerType} object.
+	 * 
+	 * <p>
+	 * Package private to only allow certain classes to access this method.
+	 * </p>
+	 * 
+	 * <p>
+	 * Redefined in each {@code PlayerType} instance.
+	 * </p>
+	 * 
+	 * @return The {@link Participant} which corresponds to the
+	 * 			{@code PlayerType} object.
+	 * @see Participant
+	 */
+	abstract Participant get();
+}
