6570f63e7d11e2da936830d048c35ac8e0d5e23f
Charles <simsc1@aston.ac.uk>
2018-03-08 13:33:14 +0000

    Backend networking added

---
+++ core/src/com/othello/network/Connection.java
@@ -0,0 +1,74 @@
+package com.othello.network;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Connection {
+
+	protected DataOutputStream writer;
+	protected DataInputStream reader;
+	protected Socket s;
+	public Connection(String IP) {
+		try {
+			s = new Socket(IP,6789);
+			reader = new DataInputStream(s.getInputStream());
+			writer = new DataOutputStream(s.getOutputStream());
+		} catch (IOException e) {
+			try {
+				s = new Socket("www.othellog4.com",5678);
+				reader = new DataInputStream(s.getInputStream());
+				writer = new DataOutputStream(s.getOutputStream());
+				writer.writeBytes(IP);
+				s = new Socket(reader.readUTF(),6789);
+				reader = new DataInputStream(s.getInputStream());
+				writer = new DataOutputStream(s.getOutputStream());
+			} catch (UnknownHostException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+			}
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	protected Connection() {
+	}
+
+	public void send(String output) {
+		try {
+			writer.writeUTF(output);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public String read() {
+
+		try {
+			return reader.readUTF();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+		return null;
+
+	}
+
+	public void close() {
+		try {
+			s.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
+
+
---
+++ core/src/com/othello/network/HostConnection.java
@@ -0,0 +1,42 @@
+package com.othello.network;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class HostConnection extends Connection {
+
+	ServerSocket server;
+	public HostConnection(String output) {
+		super();
+
+		try {
+			try {
+				Socket web = new Socket("www.othellog4.com",5678);
+				writer = new DataOutputStream(web.getOutputStream());
+				writer.writeUTF(output);
+			} catch (Exception e) {
+
+			}
+			server = new ServerSocket(6789);
+			s = server.accept();
+			reader = new DataInputStream(s.getInputStream());
+			writer = new DataOutputStream(s.getOutputStream());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void close() {
+		try {
+			server.close();
+			s.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
