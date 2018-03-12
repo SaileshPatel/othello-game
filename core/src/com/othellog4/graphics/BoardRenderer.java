b76574a4a5fe80ed29f88ffbb57b32fc76b140e0
Zakeria Hirsi <hirsiz@aston.ac.uk>
2018-03-12 14:53:06 +0000

    Added visualPiece inner class for animation

--- core/src/com/othellog4/graphics/BoardRenderer.java
+++ core/src/com/othellog4/graphics/BoardRenderer.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.Vector2;
@@ -16,8 +18,10 @@ import com.othellog4.game.board.Position;
 import com.othellog4.game.board.ProxyGameBoard;
 //import com.badlogic.gdx.utils.BaseScreen.VIEWPORT.FitViewport;
 import com.othellog4.screens.BaseScreen;
+
 /**
- * This class deals with rendering the board game, including all textures and sprites used. 
+ * This class deals with rendering the board game, including all textures and
+ * sprites used.
  * 
  * @see com.othellog4.screens.GameScreen GameScreen
  * 
@@ -63,11 +67,17 @@ public class BoardRenderer {
 	ShapeRenderer shape;
 	Texture whitePiece, blackPiece, emptyPiece, pieceHighlight;
 	private Texture background;
+	
+	static Texture pieceSheet;
+	static Texture[] animationFrames;
 
 	/**
-	 * The constructor for {@link com.othellog4.graphics.BoardRender BoardRender}. 
-	 * In this class, the majority of the work is initialising sprites and shapes for later use.  
-	 * @param model an instance of {@link com.othello.game.GameModel GameModel}
+	 * The constructor for {@link com.othellog4.graphics.BoardRender
+	 * BoardRender}. In this class, the majority of the work is initialising
+	 * sprites and shapes for later use.
+	 * 
+	 * @param model
+	 *            an instance of {@link com.othello.game.GameModel GameModel}
 	 */
 	public BoardRenderer(GameModel model) {
 		this.spriteBatch = BaseScreen.SPRITE_BATCH;
@@ -75,7 +85,7 @@ public class BoardRenderer {
 		this.model = model;
 
 		/*
-		 * A series of images needed 
+		 * A series of images needed
 		 */
 		background = new Texture("wood.jpeg");
 		whitePiece = GraphicsUtil.createMipMappedTex("whitepiece.png");
@@ -108,34 +118,50 @@ public class BoardRenderer {
 		timer = 0f;
 
 		generatePieceCoordinates();
+		
+		
+		pieceSheet = new Texture("animations/piece/sheet.png");
+		TextureRegion[][] tempFrames = TextureRegion.split(pieceSheet, 256, 256);
+		animationFrames = new Texture[30];
+
+		int index = 0;
+		for (int i = 0; i <= 6; i++) {
+			for (int j = 0; j <= 5; j++) {
+				animationFrames[index++] = tempFrames[i][j].getTexture();
+			}
+		}
 
 	}
 
-//	public void resize(int width, int height) {
-//		BaseScreen.VIEWPORT.update(width, height);
-//		cam.position.set(Othello.GAME_WORLD_WIDTH / 2, Othello.GAME_WORLD_HEIGHT / 2, 0);
-//	}
+	// public void resize(int width, int height) {
+	// BaseScreen.VIEWPORT.update(width, height);
+	// cam.position.set(Othello.GAME_WORLD_WIDTH / 2, Othello.GAME_WORLD_HEIGHT
+	// / 2, 0);
+	// }
 
 	/**
 	 * 
 	 */
 	public void update() {
 		updatePosUnderMouse();
-		//System.out.println(posUnderMouse);
+		// System.out.println(posUnderMouse);
 	}
-	
+
 	/**
-	 * Deals with rendering the game board, and it renders the board itself, colours, the game grid and 
-	 * the wooden background.
+	 * Deals with rendering the game board, and it renders the board itself,
+	 * colours, the game grid and the wooden background.
+	 * 
 	 * @see com.othellog4.screens.GameScreen GameScreen
-	 * @param delta used to increment the {@link com.othellog4.graphics.BoardRender#Timer Timer}
+	 * @param delta
+	 *            used to increment the
+	 *            {@link com.othellog4.graphics.BoardRender#Timer Timer}
 	 */
 	public void render(float delta) {
 		timer += delta;
-//		cam.update();
+		// cam.update();
 		BaseScreen.VIEWPORT.apply();
-//		spriteBatch.setProjectionMatrix(cam.combined);
-//		shape.setProjectionMatrix(cam.combined);
+		// spriteBatch.setProjectionMatrix(cam.combined);
+		// shape.setProjectionMatrix(cam.combined);
 
 		spriteBatch.begin();
 		Gdx.gl.glClearColor(0, 0, 0, 1);
@@ -143,30 +169,32 @@ public class BoardRenderer {
 		spriteBatch.end();
 
 		shape.begin(ShapeType.Filled);
-		
+
 		// wooden background
 		spriteBatch.begin();
-		spriteBatch.draw(background, 0,0, 1600, 900);
-		spriteBatch.end();		
+		spriteBatch.draw(background, 0, 0, 1600, 900);
+		spriteBatch.end();
 
 		// Dark green background
-		shape.setColor(0.01f, 0.2f, 0.022f, 1); // this line ensures that the border is kept - do not remove
+		shape.setColor(0.01f, 0.2f, 0.022f, 1); // this line ensures that the
+												// border is kept - do not
+												// remove
 		shape.rect(boardBackgroundX, boardBackgroundY, boardBackgroundWidth, boardBackgroundWidth);
 		shape.setColor(0.02f, 0.4f, 0.043f, 1);
 
 		// Light green inner
-		shape.rect(startingPosX,startingPosY - boardWidth,boardWidth,boardWidth);
+		shape.rect(startingPosX, startingPosY - boardWidth, boardWidth, boardWidth);
 		shape.setColor(0.01f, 0.2f, 0.022f, 1);
-		
-		//shape.setColor(1.00f, 0.2f, 0.022f, 1);
+
+		// shape.setColor(1.00f, 0.2f, 0.022f, 1);
 		float startingY = startingPosY - boardWidth;
 
-		//Draw board lines
-		for(int x = 0; x <= boardSize ; x++){
-			shape.rect((startingPosX + (x*columnWidth) - (lineWidth/2)),startingY,lineWidth,boardWidth);
+		// Draw board lines
+		for (int x = 0; x <= boardSize; x++) {
+			shape.rect((startingPosX + (x * columnWidth) - (lineWidth / 2)), startingY, lineWidth, boardWidth);
 		}
-		for (int y = 0 ; y <= boardSize; y++){
-			shape.rect(startingPosX,(startingY + (y*columnWidth) - (lineWidth/2)),boardWidth,lineWidth);
+		for (int y = 0; y <= boardSize; y++) {
+			shape.rect(startingPosX, (startingY + (y * columnWidth) - (lineWidth / 2)), boardWidth, lineWidth);
 
 		}
 		shape.end();
@@ -178,7 +206,7 @@ public class BoardRenderer {
 				Optional<Piece> optional = board.view(Position.at(x, y));
 
 				if (optional.isPresent() == false) {
-					//actualPiece = emptyPiece;
+					// actualPiece = emptyPiece;
 				} else {
 					Piece piece = optional.get();
 					switch (piece) {
@@ -190,37 +218,37 @@ public class BoardRenderer {
 						break;
 					}
 
-					spriteBatch.draw(actualPiece, pieceXPositions[x],
-							pieceYPositions[y], pieceSizeActual, pieceSizeActual);
+					spriteBatch.draw(actualPiece, pieceXPositions[x], pieceYPositions[y], pieceSizeActual,
+							pieceSizeActual);
 				}
-
 
 				// Draw tutorial highlight
-				//spriteBatch.setColor(1.0f, 1.0f, 1.0f, Math.abs(((float) Math.sin(timer*3 +1))));
-				if(tutorialHighlights[x][y]) {
-					spriteBatch.draw(pieceHighlight, pieceXPositions[x],
-							pieceYPositions[y], pieceSizeActual, pieceSizeActual);
+				// spriteBatch.setColor(1.0f, 1.0f, 1.0f, Math.abs(((float)
+				// Math.sin(timer*3 +1))));
+				if (tutorialHighlights[x][y]) {
+					spriteBatch.draw(pieceHighlight, pieceXPositions[x], pieceYPositions[y], pieceSizeActual,
+							pieceSizeActual);
 				}
-				//spriteBatch.setColor(1.0f, 1.0f, 1.0f, 1.0f);
+				// spriteBatch.setColor(1.0f, 1.0f, 1.0f, 1.0f);
 			}
 		}
 
 		Texture hoverTexture = null;
 
-		switch(model.getCurrentPiece()) {
-			case PIECE_A:
-				hoverTexture = blackPiece;
-				break;
-			case PIECE_B:
-				hoverTexture = whitePiece;
-				break;
+		switch (model.getCurrentPiece()) {
+		case PIECE_A:
+			hoverTexture = blackPiece;
+			break;
+		case PIECE_B:
+			hoverTexture = whitePiece;
+			break;
 		}
 
-		//Draw mouse highlight
+		// Draw mouse highlight
 		spriteBatch.setColor(1.0f, 1.0f, 1.0f, 0.5f);
-		if(drawHighlight && posUnderMouse != null && !board.view(posUnderMouse).isPresent()) {
-			spriteBatch.draw(hoverTexture, pieceXPositions[posUnderMouse.col],
-					pieceYPositions[posUnderMouse.row], pieceSizeActual, pieceSizeActual);
+		if (drawHighlight && posUnderMouse != null && !board.view(posUnderMouse).isPresent()) {
+			spriteBatch.draw(hoverTexture, pieceXPositions[posUnderMouse.col], pieceYPositions[posUnderMouse.row],
+					pieceSizeActual, pieceSizeActual);
 		}
 		spriteBatch.setColor(1.0f, 1.0f, 1.0f, 1.0f);
 		spriteBatch.end();
@@ -228,7 +256,9 @@ public class BoardRenderer {
 	}
 
 	/**
-	 * Return the board position of the piece the mouse is currently hovering over
+	 * Return the board position of the piece the mouse is currently hovering
+	 * over
+	 * 
 	 * @return Position
 	 */
 	public Position getPosUnderMouse() {
@@ -236,37 +266,35 @@ public class BoardRenderer {
 	}
 
 	/**
-	 * Update the board position of the piece the mouse is currently hovering over
-	 * Used internally for rendering piece highlights
+	 * Update the board position of the piece the mouse is currently hovering
+	 * over Used internally for rendering piece highlights
 	 */
 	private void updatePosUnderMouse() {
 		Vector2 mousePos = GraphicsUtil.getMousePos();
-		//System.out.println(mousePos);
+		// System.out.println(mousePos);
 		Integer xPos = null;
 		Integer yPos = null;
 
-		//Find if within x bounds of piece
-		for(int x = 0; x<pieceXPositions.length; x++) {
-			if(mousePos.x >= pieceXPositions[x] &&
-					mousePos.x <= pieceXPositions[x] + pieceSizeActual) {
+		// Find if within x bounds of piece
+		for (int x = 0; x < pieceXPositions.length; x++) {
+			if (mousePos.x >= pieceXPositions[x] && mousePos.x <= pieceXPositions[x] + pieceSizeActual) {
 				xPos = x;
 				break;
 			}
 		}
-		if(xPos == null) {
+		if (xPos == null) {
 			posUnderMouse = null;
 			return;
 		}
 
-		//Find if within y bounds of piece
-		for(int y = 0; y<pieceYPositions.length; y++) {
-			if(mousePos.y >= pieceYPositions[y] &&
-					mousePos.y <= pieceYPositions[y] + pieceSizeActual) {
+		// Find if within y bounds of piece
+		for (int y = 0; y < pieceYPositions.length; y++) {
+			if (mousePos.y >= pieceYPositions[y] && mousePos.y <= pieceYPositions[y] + pieceSizeActual) {
 				yPos = y;
 				break;
 			}
 		}
-		if(yPos == null) {
+		if (yPos == null) {
 			posUnderMouse = null;
 			return;
 		} else {
@@ -277,7 +305,9 @@ public class BoardRenderer {
 
 	/**
 	 * Set whether a highlight should be drawn under the mouse cursor
-	 * @param bool Draw highlight
+	 * 
+	 * @param bool
+	 *            Draw highlight
 	 */
 	public void setDrawHighlight(boolean bool) {
 		drawHighlight = bool;
@@ -287,10 +317,10 @@ public class BoardRenderer {
 	 * Update piece position coordinate arrays
 	 */
 	private void generatePieceCoordinates() {
-		for(int x=0; x<pieceXPositions.length; x++) {
+		for (int x = 0; x < pieceXPositions.length; x++) {
 			pieceXPositions[x] = startingPosX + piecePaddingActual + (columnWidth * x);
 		}
-		for(int y=0; y<pieceYPositions.length; y++) {
+		for (int y = 0; y < pieceYPositions.length; y++) {
 			pieceYPositions[y] = startingPosY - piecePaddingActual - (columnWidth * y) - pieceSizeActual;
 		}
 	}
@@ -300,8 +330,8 @@ public class BoardRenderer {
 	}
 
 	public void resetAllPieceHighlights() {
-		for(int x=0; x<tutorialHighlights.length; x++) {
-			for(int y=0; y<tutorialHighlights[0].length; y++) {
+		for (int x = 0; x < tutorialHighlights.length; x++) {
+			for (int y = 0; y < tutorialHighlights[0].length; y++) {
 				tutorialHighlights[x][y] = false;
 			}
 		}
@@ -316,5 +346,42 @@ public class BoardRenderer {
 		blackPiece.dispose();
 		emptyPiece.dispose();
 		pieceHighlight.dispose();
+	}
+
+	/**
+	 * Inner class
+	 */
+	private class VisualPiece {
+		int state;
+		
+		public VisualPiece(Piece piece) {
+			state = getState(piece);
+		}
+
+		private void update(Piece piece) {			
+			if(getState(piece) > state){
+				state--;
+			}
+			
+			if(getState(piece) < state){
+				state++;
+			}
+		}
+
+		private Texture getTexture() {
+			return animationFrames[state];
+		}
+
+		private int getState(Piece piece) {
+			switch (piece) {
+			case PIECE_A:
+				return animationFrames.length - 1;
+			case PIECE_B:
+				return 0;
+
+			}
+			return 0;
+		}
+
 	}
 }
