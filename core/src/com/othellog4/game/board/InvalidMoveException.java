10387439fc24225fbb0b35817f7a54f468166afc
Sailesh Patel <patelsa3@aston.ac.uk>
2018-03-12 14:00:54 +0000

    added additional documentation

--- core/src/com/othellog4/game/board/InvalidMoveException.java
+++ core/src/com/othellog4/game/board/InvalidMoveException.java
@@ -1,10 +1,22 @@
 package com.othellog4.game.board;
 
+/**
+ * A new exception which builds upon {@link java.lang.Exception Exception} for invalid moves
+ * @author James Shorthouse
+ * @since 29/11/2017
+ *
+ */
 public class InvalidMoveException extends Exception {
 
 	//private Piece type;
 	private Position cord;
 
+	/**
+	 * The constructor for this exeception. It calls {@link java.lang.Exception#Exception parent constructor}
+	 * @param cord the {@link com.othellog4.game.board.Position Position} of the move
+	 * @param type the type of {@link com.othellog4.game.board.Piece Piece} being moved to the 
+	 * {@link com.othellog4.game.board.Position Position}
+	 */
 	public InvalidMoveException(Position cord ,Piece type){
 		super();
 		//this.type = type;
--- desktop/src/com/othellog4/desktop/DesktopLauncher.java
+++ desktop/src/com/othellog4/desktop/DesktopLauncher.java
@@ -4,8 +4,22 @@ import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.othellog4.Othello;
-
+/**
+ * Initialises the game and renders everything on screen. 
+ * <br>
+ * Also sets the desktop icons and deals with the default screen height/width
+ * 
+ * @author James Shorthouse
+ * @since 27/11/2017
+ *
+ */
 public class DesktopLauncher {
+	/**
+	 * The main class allows the game to be launched and run properly. 
+	 * <br>
+	 * It also sets the desktop icon as well as the defaults for a LibGDX application
+	 * @param arg
+	 */
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		System.setProperty(
