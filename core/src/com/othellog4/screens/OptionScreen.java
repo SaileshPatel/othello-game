c654c455abb43c8bea93885c1768b195b87cfe44
Zakeria Hirsi <hirsiz@aston.ac.uk>
2018-03-13 13:21:28 +0000

    improved options menu with slider for sound

--- core/src/com/othellog4/screens/OptionScreen.java
+++ core/src/com/othellog4/screens/OptionScreen.java
@@ -15,8 +15,9 @@ import com.othellog4.game.GameModel;
 import com.othellog4.graphics.GraphicsUtil;
 
 /**
- * Provides an option menu for users to adjust the background music volume and whether sound effects should be 
- * on or off
+ * Provides an option menu for users to adjust the background music volume and
+ * whether sound effects should be on or off
+ * 
  * @author Zak Hirsi
  * @author BRUNO ZORIMA
  * @since 06/02/2018
@@ -31,13 +32,12 @@ public class OptionScreen extends BaseScreen {
 
 	private boolean isOn;
 	private boolean isOff;
-	
+
 	int buttonWidth = 100;
 	int buttonHeight = 100;
 	int xPosition = 0;
 	int yPosition = Othello.GAME_WORLD_HEIGHT - buttonHeight;
 	private Texture backButton;
-
 
 	Texture background;
 	Texture greyPiece;
@@ -54,7 +54,7 @@ public class OptionScreen extends BaseScreen {
 
 		isOn = true;
 		isOff = false;
-		
+
 		backButton = GraphicsUtil.createMipMappedTex("backButton.png");
 
 		FreeTypeFontGenerator titlegenerator = new FreeTypeFontGenerator(Gdx.files.internal("segoeuib.ttf"));
@@ -101,115 +101,16 @@ public class OptionScreen extends BaseScreen {
 		// Align.left, true);
 		optionsFont.draw(SPRITE_BATCH, "Music", 400, 400, 500, Align.left, true);
 		optionsFont.draw(SPRITE_BATCH, "Sound Effects", 400, 300, 500, Align.left, true);
-		optionsFont.draw(SPRITE_BATCH, "Back", 0, 40, 500, Align.left, true);
 
-		optionsFont.draw(SPRITE_BATCH, "On", 800, 300, 500, Align.left, true);
-		optionsFont.draw(SPRITE_BATCH, "Off", 950, 300, 500, Align.left, true);
-		
-
-
-		// mouse hover settings for the sound effects
-		if (mousePos.x > 800 && mousePos.x < 864 && mousePos.y < 300 && mousePos.y > 260) {
-			if (Gdx.input.justTouched()) {
-				isOn = true;
-				isOff = false;
-			}
-		}
-		if (mousePos.x > 950 && mousePos.x < 1025 && mousePos.y < 300 && mousePos.y > 260) {
-			if (Gdx.input.justTouched()) {
-				isOn = false;
-				isOff = true;
-			}
-		}
-
-		//mouse hover settings for the sound effects
-				if (mousePos.x > 800 && mousePos.x < 864 && mousePos.y < 300 && mousePos.y > 260) {
-					if (Gdx.input.justTouched()) {
-						isOn=true;
-						isOff=false;
-					}
-				}
-				if (mousePos.x > 950 && mousePos.x < 1025 && mousePos.y < 300 && mousePos.y > 260) {
-					if (Gdx.input.justTouched()) {		
-						isOn=false;
-						isOff=true;
-					}
-				}
-				
-
-				if (isOn) {
-					optionsFont.setColor(0.83f, 0.94f, 0.68f, 1f);
-					optionsFont.draw(SPRITE_BATCH, "On", 800, 300, 500, Align.left, true);
-				}
-				if (isOff) {
-					optionsFont.setColor(0.83f, 0.94f, 0.68f, 1f);
-					optionsFont.draw(SPRITE_BATCH, "Off", 950, 300, 500, Align.left, true);
-				}
-				//back button logic to exit the screen
-				if (mousePos.x > xPosition && mousePos.x < xPosition + buttonWidth && mousePos.y > yPosition && mousePos.y < yPosition + buttonHeight) {
-					if(Gdx.input.justTouched()){
-						this.dispose();
-						othello.switchToMenu();
-					}
-				}
-
-//				if (mousePos.x > 0 && mousePos.x < 110 && mousePos.y < 40 && mousePos.y > 0) {
-//					optionsFont.setColor(0.83f, 0.94f, 0.68f, 1f);
-//					optionsFont.draw(SPRITE_BATCH, "Back", 0, 40, 500, Align.left, true);
-//					if (Gdx.input.justTouched()) {
-//						this.dispose();
-//						othello.switchToMenu();
-//					}
-//				}
-				
-
-				
-		//System.out.println(mousePos.x + " " + mousePos.y);
-		// mouse hover settings for the difficults settings
-//		if (mousePos.x > 800 && mousePos.x < 904 && mousePos.y < 200 && mousePos.y > 150) {
-//			if (Gdx.input.justTouched()) {
-//				isEasy = true;
-//				isMedium = false;
-//				isHard = false;
-//			}
-//		}
-//		if (mousePos.x > 950 && mousePos.x < 1143 && mousePos.y < 200 && mousePos.y > 150) {
-//			if (Gdx.input.justTouched()) {
-//				isEasy = false;
-//				isMedium = true;
-//				isHard = false;
-//			}
-//		}
-//		if (mousePos.x > 1187 && mousePos.x < 1300 && mousePos.y < 200 && mousePos.y > 150) {
-//			if (Gdx.input.justTouched()) {
-//				isEasy = false;
-//				isMedium = false;
-//				isHard = true;
-//			}
-//		}
-//
-//		if (isEasy) {
-//			optionsFont.setColor(0.83f, 0.94f, 0.68f, 1f);
-//			optionsFont.draw(SPRITE_BATCH, "Easy", 800, 200, 500, Align.left, true);
-//		}
-//		if (isMedium) {
-//			optionsFont.setColor(0.83f, 0.94f, 0.68f, 1f);
-//			optionsFont.draw(SPRITE_BATCH, "Medium", 950, 200, 500, Align.left, true);
-//		}
-//		if (isHard) {
-//			optionsFont.setColor(0.83f, 0.94f, 0.68f, 1f);
-//			optionsFont.draw(SPRITE_BATCH, "Hard", 1185, 200, 500, Align.left, true);
-//		}
-
-		
-		if (mousePos.x > 0 && mousePos.x < 110 && mousePos.y < 40 && mousePos.y > 0) {
-			optionsFont.setColor(0.83f, 0.94f, 0.68f, 1f);
-			optionsFont.draw(SPRITE_BATCH, "Back", 0, 40, 500, Align.left, true);
+		// back button logic to exit the screen
+		if (mousePos.x > xPosition && mousePos.x < xPosition + buttonWidth && mousePos.y > yPosition
+				&& mousePos.y < yPosition + buttonHeight) {
 			if (Gdx.input.justTouched()) {
 				this.dispose();
 				othello.switchToMenu();
 			}
 		}
+
 
 		// Slider for music
 
